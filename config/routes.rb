2f1f87afd38d4e0fad304ffcb85756267856ac2d
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-25 07:23:23 +0000

    Fixed the routing for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      passwords: 'users/passwords'
+    }
+  end
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
