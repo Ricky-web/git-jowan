825be63db50e839f2962e593fe60c2708623bd47
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-25 06:57:50 +0000

    Validate for nickname column

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validats :nickname,
+  presence: true, uniqueness: true, length: {maximum: 10}
+  
 end
