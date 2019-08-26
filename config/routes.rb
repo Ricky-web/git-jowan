687b9d8812569bd373271334dbbfc8af477d80cd
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-26 10:46:12 +0000

    Defined the nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
       passwords: 'users/passwords'
     }
   end
-  resources :posts
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
