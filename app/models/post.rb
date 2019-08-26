0870116049392678e7a47a734aac0886d8c22d71
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-26 09:54:44 +0000

    Defined the validation for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     has_rich_text :content
     belongs_to :user
+    
+    [:title, :currency_pair, :user_id].each do |v|
+        validates v, presence: true
+    end
 end
