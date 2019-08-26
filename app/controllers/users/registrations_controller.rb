7c8cd456c792093967c0f9c88c78794a430cd9b4
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-26 02:51:36 +0000

    Fixed the strong parameter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :user_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image])
   end
 
   # The path used after sign up.
