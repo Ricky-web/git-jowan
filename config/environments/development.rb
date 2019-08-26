cbfe35f70ccf191a9a37cd5c52504155c1e341a1
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-08-26 04:32:11 +0000

    Re-configured the mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts << "bbd5ad31f8ca4532ad52863896ea56ad.vfs.cloud9.ap-southeast-1.amazonaws.com"
-  config.action_mailer.default_url_options = { host: 'https://bbd5ad31f8ca4532ad52863896ea56ad.vfs.cloud9.ap-southeast-1.amazonaws.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://bbd5ad31f8ca4532ad52863896ea56ad.vfs.cloud9.ap-southeast-1.amazonaws.com/' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
