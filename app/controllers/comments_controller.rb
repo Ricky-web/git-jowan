772376bc2abf0359b075e339a1e935f4c4b7bb50
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-09-02 07:37:18 +0000

    Defined actions for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+    
+    def create
+        Comment.create(create_params)
+        redirect_back(fallback_location: root_path)
+    end
+    
+    def destroy
+        comment = Comment.find(params[:id])
+        comment.destroy
+        redirect_back(fallback_location: root_path)
+    end
+    
+    private
+    def create_params
+        params.permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+    end
+    
 end
