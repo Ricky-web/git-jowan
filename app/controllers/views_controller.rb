ffd38964214ae37bf374c97c3dadcd6aeddc5ed7
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-09-05 02:01:54 +0000

    Fixed views controller

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,6 @@
 class ViewsController < ApplicationController
     def create
         @view = View.create(post_id: params[:post_id], user_id: current_user.id)
-        redirect_back(fallback_location: root_path)
+        redirect_to controller: 'posts', action: 'show', id: params[:post_id]
     end
 end
