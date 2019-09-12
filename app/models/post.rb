662d705955c8e19ec26c2e511c15ef34878940d4
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-09-12 03:40:50 +0000

    Fixed create_ranks method for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,17 @@ class Post < ApplicationRecord
     end
     
     def self.create_ranks
-        # @ranks = Like.group(:post_id).order('count_post_id DESC').limit(5).count(:post_id).keys.map{|post_id| Post.find(post_id)}
-        self.find(Like.group(:post_id).order('count(post_id) DESC').limit(20).pluck(:post_id))
+    
+        start_date = Time.current.beginning_of_day
+        end_date = Time.current.end_of_day
+        
+       daily_posts = Post.where(created_at: start_date..end_date)
+       daily_likes = daily_posts.map{|post| [post.id, post.likes.count]}.to_h
+       
+       daily_post_ranks = Hash[daily_likes.sort_by{|_, v| -v}].keys.slice(0..20)
+       result = daily_post_ranks.map{|post_id| Post.find(post_id)}
+       
+       return result
+       
     end
 end
