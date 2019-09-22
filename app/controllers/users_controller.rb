a943292edc7f6b1b4ba60f2051e0428199147aff
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-09-22 05:40:51 +0000

    Defined update action for users controller and the routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by(id: params[:id])
     end
+    
+    def update
+        user = User.find(params[:id])
+        user.update(update_params)
+        redirect_back(fallback_location: root_path)
+    end
+    
+    private
+    def update_params
+        params.require(:user).permit(:nickname)
+    end
 end
