1aefb8262928b635a1c5a0948e701cca6364e201
EC2 Default User <ec2-user@ip-172-31-0-105.ap-southeast-1.compute.internal>
2019-10-04 08:49:00 +0000

    Fixed views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  # before_action :set_post, only: [:show, :edit, :update, :destroy]
-  # before_action :ranks, only: [:top]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :ranks, only: [:top]
 
   # GET /posts
   # GET /posts.json
